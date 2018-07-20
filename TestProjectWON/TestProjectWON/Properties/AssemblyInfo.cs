9c8b8afb00d39c7f512713cb1d1d8df9a6574ec2
Navin <navb@microsoft.com>
2018-07-20 09:56:07 +0530

    Test Project w/o nuget dependency

---
+++ TestProjectWON/TestProjectWON/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProjectWON")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProjectWON")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d3c288a-e3d3-4fbe-b906-de01aa758729")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProjectWON/TestProjectWON/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectWON
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod0()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod01()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod21()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod31()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod02()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod22()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod32()
+        {
+            Assert.AreEqual(2, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod03()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Assert.AreEqual(2, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod23()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod33()
+        {
+            Assert.AreEqual(2, 4);
+        }
+    }
+}
