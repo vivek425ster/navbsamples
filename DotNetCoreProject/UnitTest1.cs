c50c8e34f30e4774e52b7db328a96f4d51938bdc
Navin <navb@microsoft.com>
2020-02-06 11:03:06 +0530

    fixing

---
+++ DotNetCoreProject/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetCoreProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod111()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1111()
+        {
+            Assert.AreEqual(1, 0);
+        }
+    }
+}
